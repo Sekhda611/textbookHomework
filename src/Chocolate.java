3479eae0c45358e1c968d9f50dd6f733c0f3b82e
ssekhda <ssekhda@ggc.edu>
2023-01-26 11:55:31 -0500

    initial commit

---
+++ src/Chocolate.java
@@ -0,0 +1,20 @@
+/**
+ * @class Chocolate
+ * @author Sonal Sekhda
+ * @course ITEC 2140 - 04, spring 2023
+ * @written January 26,2023
+ */
+public class Chocolate {
+    public static void main(String[] args){
+        int bagsOfChocolate = 9;
+        int eachBagOfChocolates = 2;
+        int totalOfChocolate;
+        totalOfChocolate = bagsOfChocolate * eachBagOfChocolates;
+        System.out.println("total number of chocolate  = " + totalOfChocolate);
+         int newBagOfChocolate;
+         newBagOfChocolate = totalOfChocolate / 3 ;
+         System.out.println("number of bags = " + newBagOfChocolate);
+
+
+    }
+}
---
+++ src/Stamps.java
@@ -0,0 +1,12 @@
+/**
+ * @classs SpeedLight
+ * @author: Sonal Sekhda
+ * @course ITEC 2140 -04, Spring 2023
+ * @written: January 26,2023
+ **/
+public class Stamps {
+    public static void main(String[] args){
+        int endangerdAniStamps:
+        int
+    }
+}
